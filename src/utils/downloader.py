c853c4ca16d93cdc623c60cec49e15c1eeb050b6
yarle08 <jarayarleni8@gmail.com>
2025-08-20 07:08:01 -0500

    función descarga

--- src/utils/downloader.py
+++ src/utils/downloader.py
@@ -1,8 +1,13 @@
 import yt_dlp
 import os
+from flask import current_app
 
-def download_youtube_audio(url, output_path='downloads'):
+def download_youtube_audio(url):
     try:
+        # Usamos la ruta absoluta de la app
+        output_path = current_app.config.get("DOWNLOADS_PATH", "downloads")
+        os.makedirs(output_path, exist_ok=True)  # por si no existe
+
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': f'{output_path}/%(title)s.%(ext)s',
@@ -18,6 +23,7 @@ def download_youtube_audio(url, output_path='downloads'):
             filename = f"{info['title']}.mp3"
             return filename
     except Exception as e:
+        print(f"Error al descargar: {e}")
         return None
 
 def download_music(url):
